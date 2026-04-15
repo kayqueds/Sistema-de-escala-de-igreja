744a9c136d7fe46851d1c261907418ecc4f9fa62
Kayque Estevão <kayque2911@gmail.com>
2026-04-15 15:09:36 -0300

    fix: consertando rotas, realizando navegação via react-router-dom

---
+++ frontend/src/components/common/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>
+        &copy; 2026 Sistema de Escala da Igreja. Todos os direitos reservados.
+      </p>
+    </footer>
+  );
+};
---
+++ frontend/src/pages/cadastro/Cadastro.jsx
@@ -0,0 +1,11 @@
+import "./Cadastro.css";
+
+const Cadastro = () => {
+  return (
+    <>
+      <h1>Cadastro</h1>
+      <p>Crie sua conta para acessar o sistema.</p>
+    </>
+  );
+};
+export default Cadastro;
