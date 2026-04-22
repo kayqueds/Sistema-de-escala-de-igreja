073559c92fa9199d908a8c6de40ea8abfd51296f
Kayque Estevão <kayque2911@gmail.com>
2026-04-22 11:12:33 -0300

    feat: abrindo site, fazendo script pra criar coleção de membros no firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 
-import { db } from './services/firebase';
-import { useEffect } from 'react';  // Se não estiver importado ainda
+import { useState, useEffect } from 'react';  // Se não estiver importado ainda
 import { BrowserRouter } from "react-router-dom";
 import Rotas from "./routes/Rotas.jsx";
 import { Footer } from "./components/common/footer/Footer.jsx";
 import { Header } from "./components/common/header/Header.jsx";
 import { Navigation } from "./components/common/navigation/Navigation.jsx";
-
+import { seedMembros } from './services/seedMembros';
 
 
 function App(){
+useEffect({
+    // seedMembros()
+}, [])
 
   return(
     <BrowserRouter>
