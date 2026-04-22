a49f0a410e5375369b7a52ee495fef20415c621f
Kayque Estevão <kayque2911@gmail.com>
2026-04-22 11:24:39 -0300

    fix: corrigindo problema de importação e conflito com o netlify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 
-import { useState, useEffect } from 'react';  // Se não estiver importado ainda
+import { useState, useEffect } from 'react';
 import { BrowserRouter } from "react-router-dom";
 import Rotas from "./routes/Rotas.jsx";
 import { Footer } from "./components/common/footer/Footer.jsx";
 import { Header } from "./components/common/header/Header.jsx";
 import { Navigation } from "./components/common/navigation/Navigation.jsx";
-import { seedMembros } from './services/seedMembros';
+// import { seedMembros } from './services/seedMembros';
 
 
 function App(){
